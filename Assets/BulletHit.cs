c57e75e13237a64de7b339b353299642b8c7886d
BarraSantiago <56093356+BarraSantiago@users.noreply.github.com>
2023-05-13 19:08:40 -0300

    Fixed instance Bullet Damage

--- Assets/BulletHit.cs
+++ Assets/BulletHit.cs
@@ -3,14 +3,14 @@ using UnityEngine;
 public class BulletHit : MonoBehaviour
 {
     [SerializeField] private float damage = 10;
-    private void OnTriggerEnter(Collider other)
-    {
-        if (Physics.Raycast(transform.position, transform.forward, out var hit, 1f))
-        {
-            Debug.Log(hit.transform.name);
-            Target target = hit.transform.GetComponent<Target>();
 
-            if (target != null) target.TakeDamage(damage);
+    private void OnCollisionEnter(Collision other)
+    {
+        Target target = other.transform.GetComponent<Target>();
+        if (target != null)
+        {
+            target.TakeDamage(damage);
+            Debug.Log(target.transform.name);
         }
     }
 }
