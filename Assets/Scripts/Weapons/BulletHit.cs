4bc27acd4da59a49759d884b189a153c6fef6eeb
BarraSantiago <56093356+BarraSantiago@users.noreply.github.com>
2023-06-24 20:08:23 -0300

    added sounds

--- Assets/Scripts/Weapons/BulletHit.cs
+++ Assets/Scripts/Weapons/BulletHit.cs
@@ -1,3 +1,4 @@
+using System;
 using Targets;
 using UnityEngine;
 
@@ -7,14 +8,23 @@ namespace Weapons
     {
         [SerializeField] private float damage = 10;
 
+        private static AudioSource _audioSource;
+
+        private void Start()
+        {
+            _audioSource = GetComponent<AudioSource>();
+        }
+
         private void OnCollisionEnter(Collision other)
         {
+            _audioSource.Play();
+
             if (other.transform.TryGetComponent(out Target target))
             {
                 target.TakeDamage(damage);
             }
+
             Destroy(gameObject);
         }
-
     }
 }
